0e5780fe39f0bbc4337a959bb408907655f3d3a2
KINGSTAR_RTOS <goodmichael07@gmail.com>
2021-09-15 18:46:45 +0800

    finish move 3axis linear

--- ArmController.h
+++ ArmController.h
@@ -5,236 +5,37 @@
 #include <fstream>
 #include <cstdlib>
 #include <cmath>
+#include <queue>
 
 
 #define PI 3.1415926
 #define g 9.81
 #define HD_eff 0.7 //effiency of HD
+#define RAD2DEG  57.324
 
 using namespace std;
 
 class ArmController
 {
 private:
-	//double ang_displacement[3];
-	double Ta;
-	double Tc1;
-	double Tc2;
-	double Tt1;
-	double Tt2;
-
-	double d1;
-	double d2;
-
-	double Acc[6];
-	double Vel[6];
-	double joint_acc_1[6];
-	double joint_acc_b[6];
-	double joint_acc_2[6];
-
-	double new_mid_pose[6];
-
-	//member function
-	double rounding(double num);
-
-	array<double, 6> pre_pos;
-	array<double, 6> pre_vel;
-
-	const double MotorRatedCurrent[6] = { 21, 21, 8.8, 2.83, 3.18, 2.43 }; //Amp
-	const double MotorTorqueConst[6] = { 0.15, 0.15, 0.17, 0.157, 0.157, 0.157 }; //Nm/Arms
-	const double MotorStaticFrictionTorque[6] = { 8.5, 8.5, 10, 20, 20, 20 };
-
-	double pre_vel_1;
-	double pre_pos_1;
-	double goal_vel_1; //used for goal_vel for auxiliary torque
-	double aux_torque_1;
-	bool start_move_1;
-	bool dwell_flag_1;
-	bool move_already_1;
-	bool pos_stop_already_1;
-	bool neg_stop_already_1;
-	bool ini_flag_1;
-	int dwell_counter_1;
-	int Static_Torque_Counter_1;
-	double Trigger_pos_1;
-	double Pre_Trigger_pos_1;
-	int Trigger_counter_1;
-	double Start_move_deg_1;
-	double Stop_move_deg_1;
-	int Trigger_counter_No_1;
-	int Static_Torque_Counter_No_1;
-	int Static_Torque_Value1_1;
-	int Static_Torque_Value2_1;
-	int Dynamic_Torque_Value_1;
-	int Dynamic_Torque_Value_Interval_1;
-	int Dynamic_Torque_Low_Velocity_1;
-	int Dynamic_Torque_Hi_Velocity_1;
-	int Dwell_counter_value_1;
-	double Static_Torque_Counter_No_Percentage_1;
-
-	double pre_vel_2;
-	double pre_pos_2;
-	double goal_vel_2; //used for goal_vel for auxiliary torque
-	double aux_torque_2;
-	bool start_move_2;
-	bool dwell_flag_2;
-	bool move_already_2;
-	bool pos_stop_already_2;
-	bool neg_stop_already_2;
-	bool ini_flag_2;
-	int dwell_counter_2;
-	int Static_Torque_Counter_2;
-	double Trigger_pos_2;
-	double Pre_Trigger_pos_2;
-	int Trigger_counter_2;
-	double Start_move_deg_2;
-	double Stop_move_deg_2;
-	int Trigger_counter_No_2;
-	int Static_Torque_Counter_No_2;
-	int Static_Torque_Value1_2;
-	int Static_Torque_Value2_2;
-	int Dynamic_Torque_Value_2;
-	int Dynamic_Torque_Value_Interval_2;
-	int Dynamic_Torque_Low_Velocity_2;
-	int Dynamic_Torque_Hi_Velocity_2;
-	int Dwell_counter_value_2;
-	double Static_Torque_Counter_No_Percentage_2;
-
-	double pre_vel_3;
-	double pre_pos_3;
-	double goal_vel_3; //used for goal_vel for auxiliary torque
-	double aux_torque_3;
-	bool start_move_3;
-	bool dwell_flag_3;
-	bool move_already_3;
-	bool pos_stop_already_3;
-	bool neg_stop_already_3;
-	bool ini_flag_3;
-	int dwell_counter_3;
-	int Static_Torque_Counter_3;
-	double Trigger_pos_3;
-	double Pre_Trigger_pos_3;
-	int Trigger_counter_3;
-	double Start_move_deg_3;
-	double Stop_move_deg_3;
-	int Trigger_counter_No_3;
-	int Static_Torque_Counter_No_3;
-	int Static_Torque_Value1_3;
-	int Static_Torque_Value2_3;
-	int Dynamic_Torque_Value_3;
-	int Dynamic_Torque_Value_Interval_3;
-	int Dynamic_Torque_Low_Velocity_3;
-	int Dynamic_Torque_Hi_Velocity_3;
-	int Dwell_counter_value_3;
-	double Static_Torque_Counter_No_Percentage_3;
-
-	double pre_vel_4;
-	double pre_pos_4;
-	double goal_vel_4; //used for goal_vel for auxiliary torque
-	double aux_torque_4;
-	bool start_move_4;
-	bool dwell_flag_4;
-	bool move_already_4;
-	bool pos_stop_already_4;
-	bool neg_stop_already_4;
-	bool ini_flag_4;
-	int dwell_counter_4;
-	int Static_Torque_Counter_4;
-	double Trigger_pos_4;
-	double Pre_Trigger_pos_4;
-	int Trigger_counter_4;
-	double Start_move_deg_4;
-	double Stop_move_deg_4;
-	int Trigger_counter_No_4;
-	int Static_Torque_Counter_No_4;
-	int Static_Torque_Value1_4;
-	int Static_Torque_Value2_4;
-	int Dynamic_Torque_Value_4;
-	int Dynamic_Torque_Value_Interval_4;
-	int Dynamic_Torque_Low_Velocity_4;
-	int Dynamic_Torque_Hi_Velocity_4;
-	int Dwell_counter_value_4;
-	double Static_Torque_Counter_No_Percentage_4;
-
-	double pre_vel_5;
-	double pre_pos_5;
-	double goal_vel_5; //used for goal_vel for auxiliary torque
-	double aux_torque_5;
-	bool start_move_5;
-	bool dwell_flag_5;
-	bool move_already_5;
-	bool pos_stop_already_5;
-	bool neg_stop_already_5;
-	bool ini_flag_5;
-	int dwell_counter_5;
-	int Static_Torque_Counter_5;
-	double Trigger_pos_5;
-	double Pre_Trigger_pos_5;
-	int Trigger_counter_5;
-	double Start_move_deg_5;
-	double Stop_move_deg_5;
-	int Trigger_counter_No_5;
-	int Static_Torque_Counter_No_5;
-	int Static_Torque_Value1_5;
-	int Static_Torque_Value2_5;
-	int Dynamic_Torque_Value_5;
-	int Dynamic_Torque_Value_Interval_5;
-	int Dynamic_Torque_Low_Velocity_5;
-	int Dynamic_Torque_Hi_Velocity_5;
-	int Dwell_counter_value_5;
-	double Static_Torque_Counter_No_Percentage_5;
-
-	double pre_vel_6;
-	double pre_pos_6;
-	double goal_vel_6; //used for goal_vel for auxiliary torque
-	double aux_torque_6;
-	bool start_move_6;
-	bool dwell_flag_6;
-	bool move_already_6;
-	bool pos_stop_already_6;
-	bool neg_stop_already_6;
-	bool ini_flag_6;
-	int dwell_counter_6;
-	int Static_Torque_Counter_6;
-	double Trigger_pos_6;
-	double Pre_Trigger_pos_6;
-	int Trigger_counter_6;
-	double Start_move_deg_6;
-	double Stop_move_deg_6;
-	int Trigger_counter_No_6;
-	int Static_Torque_Counter_No_6;
-	int Static_Torque_Value1_6;
-	int Static_Torque_Value2_6;
-	int Dynamic_Torque_Value_6;
-	int Dynamic_Torque_Value_Interval_6;
-	int Dynamic_Torque_Low_Velocity_6;
-	int Dynamic_Torque_Hi_Velocity_6;
-	int Dwell_counter_value_6;
-	double Static_Torque_Counter_No_Percentage_6;
-
-	double Torque_Vector;  //test
-	bool toggle23_flag;
-	int test_time;
-
-
+	double error;
+	array<double, 6> _current_position;
+	
+	array<double, 2> u;
 public:
 	const int AxisGearRatio[6] = { 161, 161, 121, 161, 161, 101 }; //gear raio
-	const double AxisZeroPoint[6] = { 57886.1416, 10373.1702, -82.9193, -1005.4330, 32374.2535, 32975.4638 };//zero point
-	double Tt;
+	const double AxisZeroPoint[6] = { 57886.1416, 10373.1702, -82.9193, -1005.4330, 21242, 28205 };//zero point
+	const double AxisZeroPointFake[6] = {0,0,0,0,0,0};//zero point
 	double t;
 	double dt;
-	double INIT_POSITION[6];
-	bool flag;
-
-	ArmController(double* init_position);
+	queue<array<double, 3>> goal_position_q;
+	
+	bool motion_end_flag;
+	bool start_move_flag;
+	void MoveLinear(array<double, 3> &goal_pose, double &vel, double &acc);
+	void FK(array<double, 3> &position, array<double, 3> &pose);
+	void IK(array<double, 3> &position, array<double, 3> &pose);
+	array<double, 6> UpdatePosition();
+	ArmController(array<double, 6> init_position);
 	~ArmController(void);
-
-	array<double, 6> Gravity_Compensation_Torque(array<double, 6> position);
-	array<double, 6> Auxiliary_Torque(array<double, 6> pos, array<double, 6> vel);
-	double Auxiliary_Torque_Axis1(double pos, double vel);
-	double Auxiliary_Torque_Axis2(double pos, double vel);
-	double Auxiliary_Torque_Axis3(double pos, double vel);
-	double Auxiliary_Torque_Axis4(double pos, double vel);
-	double Auxiliary_Torque_Axis5(double pos, double vel);
-	double Auxiliary_Torque_Axis6(double pos, double vel);
 };
--- CobotFunction.cpp
+++ CobotFunction.cpp
@@ -13,53 +13,21 @@
 using namespace std;
 
 int CyclicTask(PVOID Context)
-{
+{   
+    
     int TargetAxis[6] = { 0, 1, 2, 3, 4, 5 };
-    array<double, 6> CurrentPosition = { 0, 0, 0, 0, 0, 0 };
-    array<double, 6> TargetTorque = { 0, 0, 0, 0, 0, 0 };
-    array<double, 6> GTorque = { 0, 0, 0, 0, 0, 0 };
-    array<double, 6> ATorque = { 0, 0, 0, 0, 0, 0 };
-    array<double, 6> CurrentVelocity = { 0, 0, 0, 0, 0, 0 };
-
     ArmController* a = (ArmController*)Context;
-#if 0 //here test what will happened when setting different percentage of torque
-    for (int i = 5; i < 6; i++)
+    array<double, 6> TargetPosition;
+    array<double, 6> CurrentPosition;
+    if (a->motion_end_flag != true)
     {
-        SetAxisTorque(TargetAxis[i], 40);
+        TargetPosition = a->UpdatePosition();
+        for (int i = 0; i < 6; i++)
+        {
+            SetAxisPosition(TargetAxis[i], TargetPosition[i]);
+        }
     }
-#else
-    for (int i = 0; i < 6; i++)
-    {
-        GetAxisPosition(TargetAxis[i], mcActualValue, &CurrentPosition[i]);
-        GetAxisVelocity(TargetAxis[i], mcActualValue, &CurrentVelocity[i]);
-    }
-#if 1
-    GTorque = a->Gravity_Compensation_Torque(CurrentPosition);//joint space
-        //Torque_Vector = sqrt((Trigger_pos_1 - Pre_Trigger_pos_1) ^ 2 + (Trigger_pos_1 - Pre_Trigger_pos_1) ^ 2);
-    double Target_Torque1 = a->Auxiliary_Torque_Axis1(CurrentPosition[0], CurrentVelocity[0]);
-    SetAxisTorque(TargetAxis[0], Target_Torque1 + GTorque[0]);
-    double Target_Torque2 = a->Auxiliary_Torque_Axis2(CurrentPosition[1], CurrentVelocity[1]);
-    SetAxisTorque(TargetAxis[1], Target_Torque2 + GTorque[1]);
-    double Target_Torque3 = a->Auxiliary_Torque_Axis3(CurrentPosition[2], CurrentVelocity[2]);
-    SetAxisTorque(TargetAxis[2], Target_Torque3 + GTorque[2]);
-    double Target_Torque4 = a->Auxiliary_Torque_Axis4(CurrentPosition[3], CurrentVelocity[3]);
-    SetAxisTorque(TargetAxis[3], Target_Torque4 + GTorque[3]);
-    double Target_Torque5 = a->Auxiliary_Torque_Axis5(CurrentPosition[4], CurrentVelocity[4]);
-    SetAxisTorque(TargetAxis[4], Target_Torque5 + GTorque[4]);
-    double Target_Torque6 = a->Auxiliary_Torque_Axis6(CurrentPosition[5], CurrentVelocity[5]);
-    SetAxisTorque(TargetAxis[5], Target_Torque6 + GTorque[5]);
-#else
-    ATorque = a->Auxiliary_Torque(CurrentPosition, CurrentVelocity);
-    GTorque = a->Gravity_Compensation_Torque(CurrentPosition);//joint space
-    for (int i = 0; i < 6; i++)
-    {
-        TargetTorque[i] = GTorque[i];
-        SetAxisTorque(TargetAxis[i], TargetTorque[i]);
-    }
-#endif
-#endif
-
-    return 0;
+    
     return 0;
 }
 
@@ -97,15 +65,17 @@ int _tmain(int argc, _TCHAR* argv[])
             RtPrintf("Failed to get axis status: 0x%x\n", Code);
             //goto Exit;
         }
-
+        cout << "axisResolution = " << axisResolution << endl;
         RtPrintf("Axis status\n  Name: %s\n  Vendor: 0x%x\n  Product: 0x%x\n  Revision: %d\n  Serial: %d\n  Alias: %d\n  Explicit ID: %d\n\n", axis.Name, axis.VendorId, axis.ProductCode, axis.RevisionNumber, axis.SerialNumber, axis.AliasAddress, axis.ExplicitId);
 
         // Convert the axis unit to degree/s
+        //axisResolution = 131072;
         Code = SetAxisCountsPerUnit(TargetAxis[i], axisResolution, 360, FALSE);
         if (Code != errNoError) {
             RtPrintf("Failed to set axis unit: 0x%x\n", Code);
             //goto Exit;
         }
+        
         Code = EnableAxisUnitConversion(TargetAxis[i], TRUE);
         if (Code != errNoError) {
             RtPrintf("Failed to enable axis unit: 0x%x\n", Code);
@@ -113,7 +83,7 @@ int _tmain(int argc, _TCHAR* argv[])
         }
 
         // Select the control mode
-        Command = WaitForCommand(1, TRUE, SetAxisControlMode(TargetAxis[i], modeDirectTor));
+        Command = WaitForCommand(1, TRUE, SetAxisControlMode(TargetAxis[i], modeDirectPos));
 
         if (!Command.Done) {
             RtPrintf("Failed to set axis control mode: 0x%x\n", Command.ErrorId);
@@ -122,28 +92,23 @@ int _tmain(int argc, _TCHAR* argv[])
 
         // After converting the unit you must update the motion and PID parameters
 
-        Code = SetAxisTorquePid(TargetAxis[i], myPid);
+        // After converting the unit you must update the motion and PID parameters
+        Code = SetAxisVelocityPid(TargetAxis[i], myPid);
         if (Code != errNoError) {
             RtPrintf("Failed to set axis PID: 0x%x\n", Code);
             //goto Exit;
         }
-
-        Code = SetAxisTorquePolarity(TargetAxis[i], mcPositive);
-
-        /*
         Code = SetAxisMotionProfile(TargetAxis[i], profileUnitPerSecond, Motion);
         if (Code != errNoError) {
             RtPrintf("Failed to set axis motion profile: 0x%x\n", Code);
             //goto Exit;
         }
         RtPrintf("Axis configured\n");
-        */
-
     }
 #endif
 
 #pragma region PowerAxis
-#if 1
+#if 0
     for (int i = 0; i < 6; i++)
     {
         // Reset Any error in the axis
@@ -162,24 +127,41 @@ int _tmain(int argc, _TCHAR* argv[])
     }
 #endif 
 #pragma endregion
-    double Init_Position[6] = { 57886.1965, 2964.5288, -5629.6417, -1005.4687, 32650.9744, 32975.4556 };// Init_Pose[6] = {729.755, 0, 634.191, -PI, 0, 0.5*PI}
+    array<double, 6> Init_Position = {0,45,45,0,90,0};
 
 #if 1
     for (int i = 0; i < 6; i++)
     {
-        GetAxisPosition(TargetAxis[i], mcSetValue, &Init_Position[i]);
+        GetAxisPosition(TargetAxis[i], mcActualValue, &Init_Position[i]);
         //RtPrintf("Init position:%d\n", (int)Init_Position[i]);
     }
 #endif
     ArmController Scorpio_Arm(Init_Position);//input goal and current position in degrees
-    //array<double, 6> Current_Position = { 57886.1965, 23610.24, -82.9193, -1005.4330, 32374.2535, 32975.4638 };
-    //Scorpio_Arm.Gravity_Compensation_Torque(Current_Position);
-
+    array<double, 3> GoalPose = { 0.2230, 0.6, 180 };
+    array < double, 3> GoalPosition;
+    
+    
+    double vel = 0.05;
+    double acc = 0.1;
+    Scorpio_Arm.MoveLinear(GoalPose, vel, acc);
+    while (1)
+    {
+        if (Scorpio_Arm.start_move_flag == true) break;
+        Sleep(10);
+    }
+    
     Code = RegisterCallback(&CyclicTask, &Scorpio_Arm);
     // Wait for motion to run in the cyclic task
-    Sleep(60000);
-
-#if 1
+    
+    while (1)
+    {
+        if (Scorpio_Arm.motion_end_flag == true) break;
+        Sleep(10);
+    }
+    
+    //Sleep(5000);
+    
+#if 0
 #pragma region PowerAxis
     // Disable the axis
 
